b4f503699c9a920793854520522421eaaa5ad48a
William Bushie <willbushie@gmail.com>
2022-03-01 19:46:02 -0600

    HW#6 - implemented child processes - merge sort does non functional, but no errors

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
 #define ARRAY_SIZE 10
 
@@ -18,22 +24,23 @@ void displayArray(int* ar, int length);
 
 int main() 
 {
-   time_t t;
-   int* array = (int*)malloc(ARRAY_SIZE * sizeof(int)); //by default malloc returns a void*
-   
-   //srand(time(&t)); // randomly seed a list
+    time_t t;
+    int* array = (int*)malloc(ARRAY_SIZE * sizeof(int)); //by default malloc returns a void*
 
-   for(int index = 0; index < ARRAY_SIZE; index++) 
-   {
-      array[index] = rand() % 20;
-   }
-   
-   //displayArray(ar, ARRAY_SIZE);
-   mergeSort(array);
-   displayArray(array, ARRAY_SIZE);
-   
-   free(array); // sys call
-   return 0;
+    //srand(time(&t)); // randomly seed a list
+
+    for(int index = 0; index < ARRAY_SIZE; index++) 
+    {
+        array[index] = rand() % 20;
+    }
+
+    //displayArray(ar, ARRAY_SIZE);
+    displayArray(array, ARRAY_SIZE);
+    mergeSort(array);
+    displayArray(array, ARRAY_SIZE);
+
+    free(array); // sys call
+    return 0;
 }
 
 // have an array display itself
@@ -62,10 +69,46 @@ void mergeSortHelper(int* array, int leftmost, int rightmost)
         int leftmostB = rightmostA + 1;
         int rightmostB = rightmost;
         
-        // recurrsive merge sort call
-        mergeSortHelper(array, leftmostA, rightmostA);
-        mergeSortHelper(array, leftmostB, rightmostB);
-        
+        // create two child process - parent waits for successful execution
+        int parent = getpid();
+        int childA, childB;
+        fork();
+        if(getpid() != parent)
+        {
+            childA = getpid();
+        }
+        if(getpid() == parent)
+        {
+            childB = fork();
+        }
+        // print statements to check process id's
+        if(getpid() == parent)
+        {
+            //printf("1. children created. childA: %d, childB: %d, parent: %d, my PID: %d\n",childA,childB,parent,getpid());
+        }
+        if(getpid() != parent)
+        {
+            //printf("children created. childA: %d, childB: %d, parent: %d, my PID: %d\n",childA,childB,parent,getpid());
+        }
+
+        // divide the operations for each child & call merge sort
+        if(getpid() != parent && getpid() == childA)
+        {
+            //printf("i'm a childA PID: %d\n",getpid());
+            mergeSortHelper(array, leftmostA, rightmostA);
+            kill(getpid(),SIGKILL);
+        }
+        else if(getpid() != parent && getpid() == childB)
+        {
+            //printf("i'm a childB PID: %d\n",getpid());
+            mergeSortHelper(array, leftmostB, rightmostB);   
+            kill(getpid(),SIGKILL);
+        }
+        else if(getpid() == parent)
+        {
+            sleep(1);
+            wait(NULL);
+        }
         
         //now merge
         int tempLength = rightmost - leftmost + 1;
