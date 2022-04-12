3a77f7cabc7ee1fbe6f1c8cb5cdc1d6f5b3aeffd
William Bushie <willbushie@gmail.com>
2022-04-11 19:45:17 -0500

    HW#10 - not complete, cannot link two clients together to share a file

--- client/client.java
+++ client/client.java
@@ -1,12 +1,16 @@
 import java.io.File;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
 
 public class client 
 {
+    // class attributes
     private ArrayList<Map<String,String>> shareableDirectory = new ArrayList<Map<String,String>>();
+    private String status = ""; // can be either "sender" or "receiver"
 
+    // reads the share directory and updates class attribute shareableDirectory
     public void updateSharableDirectory()
     {
         ArrayList<Map<String,String>> holdList = new ArrayList<Map<String,String>>();
@@ -36,6 +40,7 @@ public class client
         //System.out.println(holdList);
     }
 
+    // displays the items inside the sharable directory
     public void displayShareable()
     {
         for (int i = 0; i < this.shareableDirectory.size(); i++)
@@ -44,6 +49,7 @@ public class client
         }
     }
 
+    // checks for a file inside of the shareable directory
     public boolean checkForFile(String filename)
     {
         for (int i = 0; i < this.shareableDirectory.size(); i++)
@@ -56,9 +62,33 @@ public class client
         return false;
     }
 
+    // updates the status of the client (not used yet)
+    public void updateStatus(String type)
+    {
+        this.status = type;
+    }
+
+    // sets the DataOutputStream to another client (to send/sender)
+    public void setSender()
+    {
+
+    }
+
+    // sets the DataInputStream from another client (to download/recieve)
+    public void setReceiver()
+    {
+
+    }
+
     public static void main(String[] args) 
     {
         client c = new client();
         c.updateSharableDirectory();
+        try
+        {
+            // create new socket and connect
+            Socket s = new Socket("localhost",2222);
+        }
+        catch (Exception error){}
     }
 }
